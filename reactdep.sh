20e6e6daf1bc99a69c4ea7836bf06948115112b9
passumtornado <39583908+passumtornado@users.noreply.github.com>
2022-03-10 19:54:43 +0000

    bash script

---
+++ reactdep.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+#Update ubuntu instance
+sudo apt-get update
+
+
+#install ngnix
+sudo apt-get install nginx -y
+
+#restart nginx
+sudo systemctl restart nginx
+
+
+#install nodejs ubuntu
+sudo apt-get install curl
+curl -sL https://deb.nodesource.com/setup_14.x | sudo -E bash -
+sudo apt-get install -y nodejs
+
+#clone react app
+cd /var/www/html/
+sudo git clone https://github.com/sdg000/Mobalysis.git
+cd Mobalysis/frontend
+sudo npm install
+sudo npm run build
+
+#Copy Build files 
+sudo cp -r /var/www/html/Mobalysis/frontend/build/* /var/www/html
+sudo rm -rf index.nginx.debian.html
+
+#Restart nginx
+sudo systemctl restart nginx
